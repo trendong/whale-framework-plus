2d2660c9f3d5599f0f5ea6c76b112f59229ca21f
trendong <13716892619@163.com>
2022-02-14 12:43:21 +0800

    feat：菜单crud初始化代码

--- src/main/java/com/whale/framework/repository/mapper/krplus/SysMenuMapper.java
+++ src/main/java/com/whale/framework/repository/mapper/krplus/SysMenuMapper.java
@@ -1,7 +1,13 @@
 package com.whale.framework.repository.mapper.krplus;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.whale.framework.mybatis.core.mapper.BaseMapperX;
+import com.whale.framework.mybatis.core.query.QueryWrapperX;
+import com.whale.framework.repository.common.vo.system.menu.SysMenuListReqVO;
 import com.whale.framework.repository.model.krplus.SysMenu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,6 +17,21 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author trendong
  * @since 2021-11-27
  */
-public interface SysMenuMapper extends BaseMapper<SysMenu> {
+public interface SysMenuMapper extends BaseMapperX<SysMenu> {
+
+    default Long selectCountByParentId(Long parentId) {
+        return selectCount(new QueryWrapper<SysMenu>().eq("parent_id", parentId));
+    }
+
+    default List<SysMenu> selectList(SysMenuListReqVO reqVO) {
+        return selectList(new QueryWrapperX<SysMenu>().likeIfPresent("name", reqVO.getName())
+                .eqIfPresent("status", reqVO.getStatus()));
+    }
+
+    default SysMenu selectByParentIdAndName(Long parentId, String name) {
+        return selectOne(new QueryWrapper<SysMenu>().eq("parent_id", parentId)
+                .eq("name", name));
+    }
+
 
 }
