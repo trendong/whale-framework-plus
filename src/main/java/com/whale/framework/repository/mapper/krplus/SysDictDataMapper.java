f90d004b8994aa18c03cdf44131b60ba36284644
trendong <13716892619@163.com>
2022-02-14 17:04:25 +0800

    feat：初始化系统字典代码

--- src/main/java/com/whale/framework/repository/mapper/krplus/SysDictDataMapper.java
+++ src/main/java/com/whale/framework/repository/mapper/krplus/SysDictDataMapper.java
@@ -1,7 +1,16 @@
 package com.whale.framework.repository.mapper.krplus;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.whale.framework.mybatis.core.mapper.BaseMapperX;
+import com.whale.framework.mybatis.core.query.QueryWrapperX;
+import com.whale.framework.repository.common.vo.PageResult;
+import com.whale.framework.repository.common.vo.system.dict.SysDictDataExportReqVO;
+import com.whale.framework.repository.common.vo.system.dict.SysDictDataPageReqVO;
 import com.whale.framework.repository.model.krplus.SysDictData;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -11,6 +20,34 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author trendong
  * @since 2021-11-27
  */
-public interface SysDictDataMapper extends BaseMapper<SysDictData> {
+public interface SysDictDataMapper extends BaseMapperX<SysDictData> {
+
+    default SysDictData selectByDictTypeAndValue(String dictType, String value) {
+        return selectOne(new QueryWrapper<SysDictData>().eq("dict_type", dictType)
+                .eq("value", value));
+    }
+
+    default Long selectCountByDictType(String dictType) {
+        return selectCount("dict_type", dictType);
+    }
+
+    default PageResult<SysDictData> selectPage(SysDictDataPageReqVO reqVO) {
+        return selectPage(reqVO, new QueryWrapperX<SysDictData>()
+                .likeIfPresent("label", reqVO.getLabel())
+                .likeIfPresent("dict_type", reqVO.getDictType())
+                .eqIfPresent("status", reqVO.getStatus())
+                .orderByAsc("dict_type", "sort"));
+    }
+
+    default List<SysDictData> selectList(SysDictDataExportReqVO reqVO) {
+        return selectList(new QueryWrapperX<SysDictData>().likeIfPresent("label", reqVO.getLabel())
+                .likeIfPresent("dict_type", reqVO.getDictType())
+                .eqIfPresent("status", reqVO.getStatus()));
+    }
+
+    default boolean selectExistsByUpdateTimeAfter(Date maxUpdateTime) {
+        return selectOne(new QueryWrapper<SysDictData>().select("id")
+                .gt("update_time", maxUpdateTime).last("LIMIT 1")) != null;
+    }
 
 }
--- src/main/java/com/whale/framework/repository/mapper/krplus/SysDictTypeMapper.java
+++ src/main/java/com/whale/framework/repository/mapper/krplus/SysDictTypeMapper.java
@@ -1,7 +1,14 @@
 package com.whale.framework.repository.mapper.krplus;
 
+import com.whale.framework.mybatis.core.mapper.BaseMapperX;
+import com.whale.framework.mybatis.core.query.QueryWrapperX;
+import com.whale.framework.repository.common.vo.PageResult;
+import com.whale.framework.repository.common.vo.system.dict.SysDictTypeExportReqVO;
+import com.whale.framework.repository.common.vo.system.dict.SysDictTypePageReqVO;
 import com.whale.framework.repository.model.krplus.SysDictType;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,6 +18,30 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author trendong
  * @since 2021-11-27
  */
-public interface SysDictTypeMapper extends BaseMapper<SysDictType> {
+public interface SysDictTypeMapper extends BaseMapperX<SysDictType> {
+
+    default PageResult<SysDictType> selectPage(SysDictTypePageReqVO reqVO) {
+        return selectPage(reqVO, new QueryWrapperX<SysDictType>()
+                .likeIfPresent("name", reqVO.getName())
+                .likeIfPresent("`type`", reqVO.getType())
+                .eqIfPresent("status", reqVO.getStatus())
+                .betweenIfPresent("create_time", reqVO.getBeginCreateTime(), reqVO.getEndCreateTime()));
+    }
+
+    default List<SysDictType> selectList(SysDictTypeExportReqVO reqVO) {
+        return selectList(new QueryWrapperX<SysDictType>()
+                .likeIfPresent("name", reqVO.getName())
+                .likeIfPresent("`type`", reqVO.getType())
+                .eqIfPresent("status", reqVO.getStatus())
+                .betweenIfPresent("create_time", reqVO.getBeginCreateTime(), reqVO.getEndCreateTime()));
+    }
+
+    default SysDictType selectByType(String type) {
+        return selectOne(new QueryWrapperX<SysDictType>().eq("`type`", type));
+    }
+
+    default SysDictType selectByName(String name) {
+        return selectOne(new QueryWrapperX<SysDictType>().eq("name", name));
+    }
 
 }
